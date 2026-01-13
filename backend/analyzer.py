26b60c438da0c24bef87a9fcdb9b5d7b616f76b3
Joshua Mclean <57785477+jmclean-coder@users.noreply.github.com>
2026-01-13 21:54:43 +0000

    Add basic memory measurement

--- backend/analyzer.py
+++ backend/analyzer.py
@@ -10,3 +10,13 @@ def measure_time(func, *args, **kwargs):
     func(*args, **kwargs)
     end_time = time.time()
     return end_time - start_time
+
+def measure_memory(func, *args, **kwargs):
+    """
+    Measures the peak memory usage of a function.
+    """
+    process = psutil.Process(os.gitpid())
+    before_memory = process.memory_info().rss
+    func(*args, **kwargs)
+    after_memory = process.memory_info().rss
+    return (after_memory - before_memory) / 1024 / 1024  # Memory in MB
